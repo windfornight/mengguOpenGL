a7a8a2594eac2aaed02481568160694df7584cdb
windfornight <31735198+windfornight@users.noreply.github.com>
2019-01-16 23:52:08 +0800

    三角形的绘制

--- 02_进阶/基本介绍/scene.cpp
+++ 02_进阶/基本介绍/scene.cpp
@@ -9,7 +9,35 @@ glm::mat4 modelMatrix, viewMatrix, projectionMatrix;
 
 void Init()
 {
+	float data[] = {
+		-0.2f, -0.2f, 0.0f, 1.0f,
+		0.2f, -0.2f, 0.0f, 1.0f,
+		0.0f, 0.2f, 0.0f, 1.0f
+	};
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, data, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	int fileSize = 0;
+	unsigned char* shaderCode = LoadFileContent("Res/test.vs", fileSize);
+	GLuint vsShader = CompileShader(GL_VERTEX_SHADER, (char*)shaderCode);
+	delete shaderCode;
+	shaderCode = NULL;
+
+	shaderCode = LoadFileContent("Res/test.fs", fileSize);
+	GLuint fsShader = CompileShader(GL_FRAGMENT_SHADER, (char*)shaderCode);
+	delete shaderCode;
+	shaderCode = NULL;
+
+	program = CreateProgram(vsShader, fsShader);
+	glDeleteShader(vsShader);
+	glDeleteShader(fsShader);
+
+	positionLocation = glGetAttribLocation(program, "position");
+	modelMatrixLocation = glGetUniformLocation(program, "ModelMatrix");
+	viewMatrixLocation = glGetUniformLocation(program, "ViewMatrix");
+	projectionMatrixLocation = glGetUniformLocation(program, "ProjectionMatrix");
 }
 
 void SetViewPortSize(float width, float height)
@@ -21,4 +49,21 @@ void Draw()
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(program);
+	glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+	glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+	glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(positionLocation);  //
+	glVertexAttribPointer(positionLocation, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 4, 0);  //
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glUseProgram(0);
+
+
+
 }
