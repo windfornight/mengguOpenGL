0a073d7b44ccdd78d8af993433d42d25237fe279
windfornight <31735198+windfornight@users.noreply.github.com>
2019-04-04 09:37:43 +0800

    DXT1 image

---
+++ 02_进阶/具体介绍/005_dds/main.cpp
@@ -0,0 +1,149 @@
+#include <windows.h>
+#include <stdio.h>
+#include "glew.h"
+#include "misc.h"
+#include "model.h"
+#include "Glm/glm.hpp"
+#include "Glm/ext.hpp"
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glew32.lib")
+#pragma comment(lib,"glu32.lib")
+
+LRESULT CALLBACK GLWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hwnd,msg,wParam,lParam);
+}
+
+INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WNDCLASSEX wndClass;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = NULL;
+	wndClass.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wndClass.hIcon = NULL;
+	wndClass.hIconSm = NULL;
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc=GLWindowProc;
+	wndClass.lpszClassName = L"OpenGL";
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_VREDRAW | CS_HREDRAW;
+	ATOM atom = RegisterClassEx(&wndClass);
+
+	HWND hwnd = CreateWindowEx(NULL, L"OpenGL", L"RenderWindow", WS_OVERLAPPEDWINDOW, 100, 100, 800, 600, NULL, NULL, hInstance, NULL);
+	HDC dc = GetDC(hwnd);
+	PIXELFORMATDESCRIPTOR pfd;
+	memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_TYPE_RGBA | PFD_DOUBLEBUFFER;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 8;
+
+	int pixelFormatID = ChoosePixelFormat(dc, &pfd);
+
+	SetPixelFormat(dc,pixelFormatID,&pfd);
+
+	HGLRC rc = wglCreateContext(dc);
+	wglMakeCurrent(dc, rc);
+	glewInit();
+	GLuint program = CreateGPUProgram("res/shader/specularTexture.vs", "res/shader/specularTexture.fs");
+	GLint posLocation, texcoordLocation,normalLocation, MLocation, VLocation, PLocation,NMLocation,textureLocation;
+	posLocation = glGetAttribLocation(program, "pos");
+	texcoordLocation = glGetAttribLocation(program, "texcoord");
+	normalLocation = glGetAttribLocation(program, "normal");
+
+	MLocation = glGetUniformLocation(program, "M");
+	VLocation = glGetUniformLocation(program, "V");
+	PLocation = glGetUniformLocation(program, "P");
+	NMLocation = glGetUniformLocation(program, "NM");
+	textureLocation = glGetUniformLocation(program, "U_MainTexture");
+
+	// load obj model :  vertexes¡¢vertex count¡¢indexes¡¢index count
+	unsigned int *indexes = nullptr;
+	int vertexCount = 0, indexCount = 0;
+	VertexData*vertexes = LoadObjModel("res/model/Sphere.obj", &indexes, vertexCount, indexCount);
+	if (vertexes==nullptr)
+	{
+		printf("load obj model fail\n");
+	}
+	//obj model -> vbo & ibo
+	GLuint vbo = CreateBufferObject(GL_ARRAY_BUFFER, sizeof(VertexData) * vertexCount, GL_STATIC_DRAW, vertexes);
+	GLuint ibo = CreateBufferObject(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indexCount, GL_STATIC_DRAW, indexes);
+	GLuint mainTexture = CreateTextureFromFile("res/image/150001.dds");
+	printf("vertex count %d index count %d\n",vertexCount,indexCount);
+
+	glClearColor(0.1f, 0.4f, 0.7f,1.0f);
+	glEnable(GL_DEPTH_TEST);
+	ShowWindow(hwnd, SW_SHOW);
+	UpdateWindow(hwnd);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	float identity[] = {
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1
+	};
+	glm::mat4 model = glm::translate(0.0f,0.0f, -4.0f);
+	glm::mat4 projection = glm::perspective(45.0f, 800.0f / 600.0f, 0.1f, 1000.0f);
+	glm::mat4 normalMatrix = glm::inverseTranspose(model);
+	MSG msg;
+	float angle = 0.0f;
+	while (true)
+	{
+		if (PeekMessage(&msg,NULL,NULL,NULL,PM_REMOVE))
+		{
+			if (msg.message==WM_QUIT)
+			{
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		angle += 1.0f;
+		if (angle>360.0f)
+		{
+			angle = 0.0f;
+		}
+		model= glm::translate(0.0f, 0.0f, -4.0f)*glm::rotate(angle,0.0f,1.0f,0.0f);
+		normalMatrix = glm::inverseTranspose(model);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		glUseProgram(program); 
+		glUniformMatrix4fv(MLocation, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(VLocation, 1, GL_FALSE, identity);
+		glUniformMatrix4fv(PLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(NMLocation, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+		glBindTexture(GL_TEXTURE_2D, mainTexture);
+		glUniform1i(textureLocation, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(posLocation);
+		glVertexAttribPointer(posLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)0);
+		glEnableVertexAttribArray(texcoordLocation);
+		glVertexAttribPointer(texcoordLocation, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float) * 3));
+		glEnableVertexAttribArray(normalLocation);
+		glVertexAttribPointer(normalLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float) * 5));
+		
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glUseProgram(0);
+
+		SwapBuffers(dc);
+	}
+	return 0;
+}
---
+++ 02_进阶/具体介绍/005_dds/misc.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "glew.h"
+
+GLuint CreateBufferObject(GLenum bufferType, GLsizeiptr size, GLenum usage, void*data = nullptr);
+char *LoadFileContent(const char*path);
+GLuint CompileShader(GLenum shaderType,const char*shaderPath);
+GLuint CreateGPUProgram(const char*vsShaderPath, const char*fsShaderPath);
+GLuint CreateTextureFromFile(const char*imagePath);
