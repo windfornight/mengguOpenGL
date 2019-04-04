cffb4d5dec7e544a856316dedbb2b5150149f371
windfornight <31735198+windfornight@users.noreply.github.com>
2019-04-04 15:02:15 +0800

    刚好铺满屏幕的四边形

---
+++ 02_进阶/具体介绍/007_fullscene/cpu/main.cpp
@@ -0,0 +1,171 @@
+#include <windows.h>
+#include <stdio.h>
+#include "glew.h"
+#include "misc.h"
+#include "model.h"
+#include "Glm/glm.hpp"
+#include "Glm/ext.hpp"
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glew32.lib")
+#pragma comment(lib,"glu32.lib")
+
+LRESULT CALLBACK GLWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hwnd,msg,wParam,lParam);
+}
+
+INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WNDCLASSEX wndClass;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = NULL;
+	wndClass.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wndClass.hIcon = NULL;
+	wndClass.hIconSm = NULL;
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc=GLWindowProc;
+	wndClass.lpszClassName = L"OpenGL";
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_VREDRAW | CS_HREDRAW;
+	ATOM atom = RegisterClassEx(&wndClass);
+
+	HWND hwnd = CreateWindowEx(NULL, L"OpenGL", L"RenderWindow", WS_OVERLAPPEDWINDOW, 100, 100, 800, 600, NULL, NULL, hInstance, NULL);
+	HDC dc = GetDC(hwnd);
+	PIXELFORMATDESCRIPTOR pfd;
+	memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_TYPE_RGBA | PFD_DOUBLEBUFFER;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 8;
+
+	int pixelFormatID = ChoosePixelFormat(dc, &pfd);
+
+	SetPixelFormat(dc,pixelFormatID,&pfd);
+
+	HGLRC rc = wglCreateContext(dc);
+	wglMakeCurrent(dc, rc);
+	int width, height;
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+	width = rect.right - rect.left;
+	height = rect.bottom - rect.top;
+
+	glewInit();
+	GLuint program = CreateGPUProgram("res/shader/UI.vs", "res/shader/UI.fs");
+	GLint posLocation, texcoordLocation,normalLocation, MLocation, VLocation, PLocation,NMLocation,textureLocation;
+	posLocation = glGetAttribLocation(program, "pos");
+	texcoordLocation = glGetAttribLocation(program, "texcoord");
+	normalLocation = glGetAttribLocation(program, "normal");
+
+	MLocation = glGetUniformLocation(program, "M");
+	VLocation = glGetUniformLocation(program, "V");
+	PLocation = glGetUniformLocation(program, "P");
+	NMLocation = glGetUniformLocation(program, "NM");
+	textureLocation = glGetUniformLocation(program, "U_MainTexture");
+
+	// load obj model :  vertexes¡¢vertex count¡¢indexes¡¢index count
+	unsigned int *indexes = nullptr;
+	int vertexCount = 0, indexCount = 0;
+	VertexData*vertexes = LoadObjModel("res/model/Quad.obj", &indexes, vertexCount, indexCount);
+	if (vertexes==nullptr)
+	{
+		printf("load obj model fail\n");
+	}
+	//-0.5~0.5
+	float z = 4.0f;
+	float halfFOV = 22.5f;
+	float randianHalfFOV = 3.14f*halfFOV / 180.0f;
+	float tanHalfFOV = sinf(randianHalfFOV) / cosf(randianHalfFOV);
+	float y = tanHalfFOV*z;
+	float aspect = (float)width / (float)height;
+	float x = y*aspect;
+	//left bottom
+	vertexes[0].position[0] = -x;
+	vertexes[0].position[1] = -y;
+	//right bottom
+	vertexes[1].position[0] = x;
+	vertexes[1].position[1] = -y;
+	//left top
+	vertexes[2].position[0] = -x;
+	vertexes[2].position[1] = y;
+	//right top
+	vertexes[3].position[0] = x;
+	vertexes[3].position[1] = y;
+
+	//obj model -> vbo & ibo
+	GLuint vbo = CreateBufferObject(GL_ARRAY_BUFFER, sizeof(VertexData) * vertexCount, GL_STATIC_DRAW, vertexes);
+	GLuint ibo = CreateBufferObject(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indexCount, GL_STATIC_DRAW, indexes);
+	GLuint mainTexture = CreateTextureFromFile("res/image/niutou.bmp");
+	printf("vertex count %d index count %d\n",vertexCount,indexCount);
+
+	glClearColor(0.1f, 0.4f, 0.7f,1.0f);
+	glEnable(GL_DEPTH_TEST);
+	ShowWindow(hwnd, SW_SHOW);
+	UpdateWindow(hwnd);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glViewport(0, 0, width,height);
+	float identity[] = {
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1
+	};
+
+
+	glm::mat4 model = glm::translate(0.0f,0.0f,-4.0f);
+	glm::mat4 projection = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
+	//glm::mat4 uiMatrix = glm::ortho(-400.0f, 400.0f,-300.0f,300.0f);
+	glm::mat4 normalMatrix = glm::inverseTranspose(model);
+	MSG msg;
+	while (true)
+	{
+		if (PeekMessage(&msg,NULL,NULL,NULL,PM_REMOVE))
+		{
+			if (msg.message==WM_QUIT)
+			{
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		glUseProgram(program); 
+		glUniformMatrix4fv(MLocation, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(VLocation, 1, GL_FALSE, identity);
+		glUniformMatrix4fv(PLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(NMLocation, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+		glBindTexture(GL_TEXTURE_2D, mainTexture);
+		glUniform1i(texcoordLocation, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(posLocation);
+		glVertexAttribPointer(posLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)0);
+		glEnableVertexAttribArray(texcoordLocation);
+		glVertexAttribPointer(texcoordLocation, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float) * 3));
+		glEnableVertexAttribArray(normalLocation);
+		glVertexAttribPointer(normalLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float) * 5));
+		
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glUseProgram(0);
+
+		SwapBuffers(dc);
+	}
+	return 0;
+}
---
+++ 02_进阶/具体介绍/007_fullscene/cpu/misc.cpp
@@ -0,0 +1,185 @@
+#include "misc.h"
+#include <stdio.h>
+#include <string.h>
+
+GLuint CreateBufferObject(GLenum bufferType, GLsizeiptr size, GLenum usage, void*data /* = nullptr */)
+{
+	GLuint object;
+	glGenBuffers(1, &object);
+	glBindBuffer(bufferType, object);
+	glBufferData(bufferType, size, data, usage);
+	glBindBuffer(bufferType, 0);
+	return object;
+}
+
+
+char *LoadFileContent(const char*path)
+{
+	FILE*pFile = fopen(path, "rb");
+	if (pFile)
+	{
+		fseek(pFile, 0, SEEK_END);
+		int nLen = ftell(pFile);
+		char*buffer = nullptr;
+		if (nLen!=0)
+		{
+			buffer=new char[nLen + 1];
+			rewind(pFile);
+			fread(buffer, nLen, 1, pFile);
+			buffer[nLen] = '\0';
+		}
+		else
+		{
+			printf("load file fail %s content len is 0\n", path);
+		}
+		fclose(pFile);
+		return buffer;
+	}
+	else
+	{
+		printf("open file %s fail\n",path);
+	}
+	fclose(pFile);
+	return nullptr;
+}
+
+
+GLuint CompileShader(GLenum shaderType, const char*shaderPath)
+{
+	GLuint shader = glCreateShader(shaderType);
+	if (shader==0)
+	{
+		printf("glCreateShader fail\n");
+		return 0;
+	}
+	const char* shaderCode = LoadFileContent(shaderPath);
+	if (shaderCode==nullptr)
+	{
+		printf("load shader code from file : %s fail\n",shaderPath);
+		glDeleteShader(shader);
+		return 0;
+	}
+	glShaderSource(shader, 1, &shaderCode, nullptr);
+	glCompileShader(shader);
+	GLint compileResult = GL_TRUE;
+	glGetShaderiv(shader,GL_COMPILE_STATUS,&compileResult);
+	if (compileResult==GL_FALSE)
+	{
+		char szLog[1024] = {0};
+		GLsizei logLen = 0;
+		glGetShaderInfoLog(shader,1024,&logLen,szLog);
+		printf("Compile Shader fail error log : %s \nshader code :\n%s\n",szLog,shaderCode);
+		glDeleteShader(shader);
+		shader = 0;
+	}
+	delete shaderCode;
+	return shader;
+}
+
+
+GLuint CreateGPUProgram(const char*vsShaderPath, const char*fsShaderPath)
+{
+	GLuint vsShader = CompileShader(GL_VERTEX_SHADER, vsShaderPath);
+	GLuint fsShader = CompileShader(GL_FRAGMENT_SHADER,fsShaderPath);
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vsShader);
+	glAttachShader(program, fsShader);
+	glLinkProgram(program);
+	glDetachShader(program, vsShader);
+	glDetachShader(program, fsShader);
+	glDeleteShader(vsShader);
+	glDeleteShader(fsShader);
+	GLint linkResult = GL_TRUE;
+	glGetProgramiv(program,GL_LINK_STATUS,&linkResult);
+	if (linkResult==GL_FALSE)
+	{
+		char szLog[1024] = { 0 };
+		GLsizei logLen = 0;
+		glGetProgramInfoLog(program, 1024, &logLen, szLog);
+		printf("link program fail error log : %s \n vs : %s\n fs : %s\n", szLog, vsShaderPath,fsShaderPath);
+		glDeleteProgram(program);
+		program = 0;
+	}
+	return program;
+}
+
+static unsigned char*DecodeBMPData(unsigned char*imgData,int&width,int&height)
+{
+	int pixelDataOffset = *((int*)(imgData + 10));
+	width = *((int*)(imgData + 18));
+	height = *((int*)(imgData + 22));
+	unsigned char*pixelData = (imgData + pixelDataOffset);
+	for (int i = 0; i < width*height * 3; i += 3)
+	{
+		//bgr->rgb
+		unsigned char temp = pixelData[i + 2];
+		pixelData[i + 2] = pixelData[i];
+		pixelData[i] = temp;
+	}
+	return pixelData;
+}
+
+const unsigned long FORMAT_DXT1 = 0x31545844l;//DXT1 -> 1 T X D
+static unsigned char*DecodeDXT1Data(unsigned char*imgData, int&width, int&height,int&pixelDataSize)
+{
+	height = *(unsigned long*)(imgData + sizeof(unsigned long) * 3);
+	width = *(unsigned long*)(imgData + sizeof(unsigned long) * 4);
+	pixelDataSize= *(unsigned long*)(imgData + sizeof(unsigned long) * 5);
+	unsigned long compressFormat;
+	compressFormat = *(unsigned long*)(imgData + sizeof(unsigned long) * 21);
+	switch (compressFormat)
+	{
+	case FORMAT_DXT1:
+		break;
+	}
+	unsigned char*pixelData = new unsigned char[pixelDataSize];
+	memcpy(pixelData,imgData + sizeof(unsigned long) * 32,pixelDataSize);
+
+	return pixelData;
+}
+
+GLuint CreateTextureFromFile(const char*imagePath)
+{
+	unsigned char*imgData = (unsigned char*)LoadFileContent(imagePath);
+
+	int width = 0;
+	int height = 0;
+	unsigned char*pixelData = nullptr;
+	int pixelDataSize = 0;
+	GLenum srcFormat = GL_RGB;
+	if (*((unsigned short*)imgData) == 0x4D42)
+	{
+		pixelData = DecodeBMPData(imgData, width, height);
+	}
+	else if (memcmp(imgData,"DDS ",4)==0)
+	{
+		pixelData = DecodeDXT1Data(imgData, width, height, pixelDataSize);
+		srcFormat = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+	}
+
+	if (pixelData==nullptr)
+	{
+		delete imgData;
+		return 0;
+	}
+
+	//decode bmp
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	if (srcFormat==GL_RGB)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixelData);
+	}
+	else if(srcFormat==GL_COMPRESSED_RGBA_S3TC_DXT1_EXT)
+	{
+		glCompressedTexImage2D(GL_TEXTURE_2D,0,srcFormat,width,height,0, pixelDataSize,pixelData);
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+	delete imgData;
+	return texture;
+}
