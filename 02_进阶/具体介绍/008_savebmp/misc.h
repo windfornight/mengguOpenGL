0be7d8337222053fa1bdcd26b5f11a6ebffcb83b
windfornight <31735198+windfornight@users.noreply.github.com>
2019-04-08 14:28:38 +0800

    保存图片接口bmp

---
+++ 02_进阶/具体介绍/008_savebmp/misc.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "glew.h"
+
+GLuint CreateBufferObject(GLenum bufferType, GLsizeiptr size, GLenum usage, void*data = nullptr);
+char *LoadFileContent(const char*path);
+GLuint CompileShader(GLenum shaderType,const char*shaderPath);
+GLuint CreateGPUProgram(const char*vsShaderPath, const char*fsShaderPath);
+GLuint CreateTextureFromFile(const char*imagePath);
+void SaveImage(const char*imagePath,unsigned char*imgData,int width,int height);
