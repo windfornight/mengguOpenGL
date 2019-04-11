bc88bbe67ce03b5fd2a894ce52dc13ce45b40a19
windfornight <31735198+windfornight@users.noreply.github.com>
2019-04-11 16:45:24 +0800

    instance+垂直同步问题

---
+++ 02_进阶/具体介绍/013_instance/main.cpp
@@ -0,0 +1,189 @@
+#include <windows.h>
+#include <stdio.h>
+#include "glew.h"
+#include "misc.h"
+#include "model.h"
+#include "Glm/glm.hpp"
+#include "Glm/ext.hpp"
+#include "timer.h"
+#include "frustum.h"
+#include "GLAUX.H"
+
+
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glew32.lib")
+#pragma comment(lib,"glu32.lib")
+#pragma comment(lib, "glaux.lib")
+
+LRESULT CALLBACK GLWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hwnd,msg,wParam,lParam);
+}
+
+INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WNDCLASSEX wndClass;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = NULL;
+	wndClass.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wndClass.hIcon = NULL;
+	wndClass.hIconSm = NULL;
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc=GLWindowProc;
+	wndClass.lpszClassName = L"OpenGL";
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_VREDRAW | CS_HREDRAW;
+	ATOM atom = RegisterClassEx(&wndClass);
+	RECT rect;
+	rect.left = 0;
+	rect.right = 800;
+	rect.bottom = 600;
+	rect.top = 0;
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindowEx(NULL, L"OpenGL", L"RenderWindow", WS_OVERLAPPEDWINDOW, 100, 100, rect.right-rect.left, rect.bottom-rect.top, NULL, NULL, hInstance, NULL);
+	HDC dc = GetDC(hwnd);
+	PIXELFORMATDESCRIPTOR pfd;
+	memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_TYPE_RGBA | PFD_DOUBLEBUFFER;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 8;
+
+	int pixelFormatID = ChoosePixelFormat(dc, &pfd);
+
+	SetPixelFormat(dc,pixelFormatID,&pfd);
+	
+	HGLRC rc = wglCreateContext(dc);
+	wglMakeCurrent(dc, rc);
+	int width, height;
+
+	GetClientRect(hwnd, &rect);
+	width = rect.right - rect.left;
+	height = rect.bottom - rect.top;
+
+	glewInit();
+
+
+	GLuint program = CreateGPUProgram("res/shader/diffuse.vs", "res/shader/diffuse.fs");
+	GLint posLocation, texcoordLocation,normalLocation, MLocation, VLocation, PLocation,NMLocation,textureLocation,offsetLocation;
+	posLocation = glGetAttribLocation(program, "pos");
+	texcoordLocation = glGetAttribLocation(program, "texcoord");
+	normalLocation = glGetAttribLocation(program, "normal");
+	offsetLocation = glGetAttribLocation(program, "offset");
+
+	MLocation = glGetUniformLocation(program, "M");
+	VLocation = glGetUniformLocation(program, "V");
+	PLocation = glGetUniformLocation(program, "P");
+	NMLocation = glGetUniformLocation(program, "NM");
+	textureLocation = glGetUniformLocation(program, "U_MainTexture");
+
+	// load obj model :  vertexes¡¢vertex count¡¢indexes¡¢index count
+	unsigned int *indexes = nullptr;
+	int vertexCount = 0, indexCount = 0;
+	VertexData*vertexes = LoadObjModel("res/model/niutou.obj", &indexes, vertexCount, indexCount);
+	if (vertexes==nullptr)
+	{
+		printf("load obj model fail\n");
+	}
+	//obj model -> vbo & ibo
+	GLuint vbo = CreateBufferObject(GL_ARRAY_BUFFER, sizeof(VertexData) * vertexCount, GL_STATIC_DRAW, vertexes);
+	GLuint ibo = CreateBufferObject(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indexCount, GL_STATIC_DRAW, indexes);
+	GLuint mainTexture = CreateTextureFromFile("res/image/niutou.bmp");
+
+
+	float posOffsets[] = {
+		-1.0f,0.0f,0.0f,
+		0.0f,0.0f,0.0f,
+		1.0f,0.0f,0.0f
+	};
+	GLuint offsetVBO=CreateBufferObject(GL_ARRAY_BUFFER, sizeof(float) * 9, GL_STATIC_DRAW, posOffsets);
+	GL_CALL(glClearColor(0.1f, 0.4f, 0.7f,1.0f));
+	glEnable(GL_DEPTH_TEST);
+	ShowWindow(hwnd, SW_SHOW);
+	UpdateWindow(hwnd);
+	glViewport(0, 0, width,height);
+	float identity[] = {
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1
+	};
+	glm::mat4 model = glm::translate(0.0f,-0.5f,-4.0f)*glm::rotate(-90.0f,0.0f,1.0f,0.0f)*glm::scale(0.01f,0.01f,0.01f);
+	glm::mat4 projection = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
+	//glm::mat4 uiMatrix = glm::ortho(-400.0f, 400.0f,-300.0f,300.0f);
+	glm::mat4 normalMatrix = glm::inverseTranspose(model);
+	MSG msg;
+	auto what = [&]()->void 
+	{
+		glUseProgram(program);
+		glUniformMatrix4fv(MLocation, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(VLocation, 1, GL_FALSE, identity);
+		glUniformMatrix4fv(PLocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(NMLocation, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+		glBindTexture(GL_TEXTURE_2D, mainTexture);
+		glUniform1i(texcoordLocation, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(posLocation);
+		glVertexAttribPointer(posLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)0);
+		glEnableVertexAttribArray(texcoordLocation);
+		glVertexAttribPointer(texcoordLocation, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float) * 3));
+		glEnableVertexAttribArray(normalLocation);
+		glVertexAttribPointer(normalLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float) * 5));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, offsetVBO);
+		glEnableVertexAttribArray(offsetLocation);
+		glVertexAttribPointer(offsetLocation, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, (void*)0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		//
+		glVertexAttribDivisor(offsetLocation,1);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glDrawElementsInstanced(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glUseProgram(0);
+	};
+	Timer t;
+
+	
+	while (true)
+	{
+		if (PeekMessage(&msg,NULL,NULL,NULL,PM_REMOVE))
+		{
+			if (msg.message==WM_QUIT)
+			{
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		t.Start();
+		model = glm::translate(0.0f, -0.5f, -4.0f)*glm::rotate(-90.0f, 0.0f, 1.0f, 0.0f)*glm::scale(0.01f, 0.01f, 0.01f);
+		//for(int i = 1; i < 100; ++i)
+		what();
+		model = glm::translate(0.0f, -0.5f, -4.0f)*glm::rotate(-90.0f, 0.0f, 1.0f, 0.0f)*glm::scale(0.01f, 0.01f, 0.01f);
+		what();
+		model = glm::translate(1.0f, -0.5f, -4.0f)*glm::rotate(-90.0f, 0.0f, 1.0f, 0.0f)*glm::scale(0.01f, 0.01f, 0.01f);
+		what();
+		//glFlush();
+		glFinish();
+		printf("cost time %fms\n",t.GetPassedTime()*1000.0f);
+		SwapBuffers(dc);
+	}
+	return 0;
+}
